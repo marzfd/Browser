7b31451fbdc54a9dad805b18beb46e89f10b4a41
Marzieh <mar.31069@gmail.com>
2021-09-22 18:06:05 +0330

    Change img src

--- week1/ex5-catWalk/index.js
+++ week1/ex5-catWalk/index.js
@@ -1,7 +1,7 @@
 const catImg = document.querySelector('body img');
 let position = 0;
 catImg.style.left = "0px"
-
+    // catImg.width is around 300
 
 function main() {
 
@@ -9,17 +9,15 @@ function main() {
     if (position > screen.width) {
       position = 0;
     }
-    // catImg.width is around 300
     else if (position == (screen.width / 2) - 150) {
       clearInterval(walking);
-      catImg.src = 'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
+      catImg.src = 'tenor.gif';
       setTimeout(() => {
-        catImg.src = 'http://www.anniemation.com/clip_art/images/cat-walk.gif';
+        catImg.src = 'cat-walk.gif';
         position += 10;
         main();
       }, 5000);
     }
-    
     else {
       catImg.style.left = `${position}px`;
       position += 10;
