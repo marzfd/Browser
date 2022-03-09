e15e5cee64c1d2c3d4c36c87e2e2f8f38eedf840
Marzieh <mar.31069@gmail.com>
2022-03-09 14:26:06 +0330

    Change directory

--- week1/ex3-hijackLogo/ex3-hijackLogo.js
+++
@@ -1,7 +0,0 @@
-function hijackLogo() {
-  const googleLogo = document.getElementById('hplogo');
-  googleLogo.src = 'https://www.hackyourfuture.dk/static/logo-dark.svg';
-  googleLogo.srcset = 'https://www.hackyourfuture.dk/static/logo-dark.svg';
-}
-
-hijackLogo();
